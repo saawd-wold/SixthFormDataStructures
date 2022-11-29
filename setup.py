22d933daccb51685960d2ca161dc95935e9bcb4d
saawd-wold <saawd@woldinghamschool.co.uk>
2022-11-29 09:56:40 +0000

    Updated version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="SixthFormDataStructures",
-    version="0.0.1",
+    version="0.0.2",
     description="A simple library of naively implemented data structures.",
     author="Daniel Sääw",
     author_email="saawd@woldinghamschool.co.uk",
