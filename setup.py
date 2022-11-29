ae0ed9dd1563bc917b3187c606fa10480b1d1450
saawd-wold <saawd@woldinghamschool.co.uk>
2022-11-29 14:07:40 +0000

    Copy skippy's setup

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup, find_packages
 
-setup(
-    name="SixthFormDataStructures",
-    version="0.0.12",
-    description="A simple library of naively implemented data structures.",
-    author="Daniel Sääw",
-    author_email="saawd@woldinghamschool.co.uk",
-    packages=find_packages("."),
-)
+def configuration(parent_package='',top_path=None):
+    from numpy.distutils.system_info import get_info
+    lapack_opt = get_info("lapack_opt")
+
+    from numpy.distutils.misc_util import Configuration
+    config = Configuration('danielsdatastructures',parent_package,top_path)
+    config.add_subpackage('queues')
+    config.make_config_py()
+    return config
+
+
+if __name__ == '__main__':
+    from numpy.distutils.core import setup
+    setup(**configuration(top_path='').todict())
