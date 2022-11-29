b3fd7701a66788cfc508d27d81cbdc82489db4ba
saawd-wold <saawd@woldinghamschool.co.uk>
2022-11-29 10:30:42 +0000

    honestly this feels like an awful hack

--- danielsdatastructures/__init__.py
+++ danielsdatastructures/__init__.py
@@ -1,3 +1,8 @@
 __path__ = ["danielsdatastructures", "danielsdatastructures/queues"]
 
+import os
+cwd = os.getcwd()
+import sys
+sys.path.append(cwd+"/queues")
+
 from danielsdatastructures.queues import Queue, CircularQueue 
