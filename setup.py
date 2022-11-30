fba83e142dc02f3f6d8221a9e98b72d18a6a6b79
saawd-wold <saawd@woldinghamschool.co.uk>
2022-11-30 12:13:17 +0000

    .

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import find_packages
 from distutils.core import setup
+from danielsdatastructures import __version__
 print("Packages found:", find_packages())
 
 setup(
     name="danielsdatastructures",
+    version=__version__
     description="A simple library of naively implemented data structures.",
     author="Daniel Sääw",
     author_email="saawd@woldinghamschool.co.uk",
