1f5ba670fefb455d5e1c1f150805d332a13c2c32
saawd-wold <saawd@woldinghamschool.co.uk>
2022-11-30 08:11:35 +0000

    class experiment

--- danielsdatastructures/Stacks/__init__.py
+++ danielsdatastructures/Stacks/__init__.py
@@ -1,3 +1,16 @@
 from . import stacks
+from abc import ABC, abstractmethod
+
+class AbstractStack(ABC):
+    @abstractmethod
+    def push(self, item):
+        pass
+    def pop(self):
+        pass
+    def isEmpty(self):
+        pass
+    def isFull(self):
+        pass
+    pass
 
 print("Stacks' x: ", stacks.x)
--- danielsdatastructures/Stacks/stacks.py
+++ danielsdatastructures/Stacks/stacks.py
@@ -1,1 +1,13 @@
 x = 5
+
+from danielsdatastructures.Stacks import AbstractStack
+
+class Stack(AbstractStack):
+    def push(x):
+        print(x)
+    def pop():
+        print("pop")
+    def isEmpty():
+        return True
+    def isFull():
+        return True
