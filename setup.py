7fb4e3c3813301c297267bd1d720d842878301f0
saawd-wold <saawd@woldinghamschool.co.uk>
2022-11-30 10:34:35 +0000

    f

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ print("Packages found:", find_packages())
 
 setup(
     name="danielsdatastructures",
-    version="0.2.1",
+    version="0.2.2",
     description="A simple library of naively implemented data structures.",
     author="Daniel Sääw",
     author_email="saawd@woldinghamschool.co.uk",
