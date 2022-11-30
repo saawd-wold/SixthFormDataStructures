20aef02dfa12cd47ecd086738635c3868289cddb
saawd-wold <saawd@woldinghamschool.co.uk>
2022-11-30 07:52:36 +0000

    final attempt to make subpackages work

--- danielsdatastructures/__init__.py
+++ danielsdatastructures/__init__.py
@@ -1,3 +1,18 @@
 from . import queues
 from . import queues
-from .Stacks import stacks
+
+submodules = ["Stacks"]
+__all__ = submodules + ["__version__"]
+
+import importlib as _importlib
+
+def __getattr__(name):
+    if name in submodules:
+        _importlib.import_module(f"danielsdatastructures.{name}")
+    else:
+        try:
+            return globals()[name]
+        except KeyError:
+            raise AttributeError(
+                f"Module 'danielsdatastructures' has no attribute '{name}'"
+            )
